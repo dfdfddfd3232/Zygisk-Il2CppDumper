23fcc27898996c5b54d1c7e7c6efda41394cfd12
dfdfddfd3232 <egorivanln237@gmail.com>
2026-07-03 21:59:09 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.je.supersus"
+#define GamePackageName "com.herogame.gplay.magicminecraft.mmorpg"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H

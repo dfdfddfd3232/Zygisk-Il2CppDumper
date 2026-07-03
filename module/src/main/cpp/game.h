cb0410ee5ad641f5ebe0fbf63a1587bf12d13e0f
dfdfddfd3232 <egorivanln237@gmail.com>
2026-07-03 23:25:04 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.herogame.gplay.magicminecraft.mmorpg"
+#define GamePackageName "com.outfit7.mytalkingtomfree"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
